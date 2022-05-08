c865b0989b444f15d82fd1e971a35a24c5b50b0b
Babu Devandla <babu.devandla524@gmail.com>
2022-05-08 12:53:07 +0530

    Initial Commits
    circuit-breaker design pattern

---
+++ spring-hystrix-student-service/src/main/java/com/example/howtodoinjava/springhystrixstudentservice/SpringHystrixStudentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.howtodoinjava.springhystrixstudentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHystrixStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHystrixStudentServiceApplication.class, args);
+	}
+}
---
+++ spring-hystrix-student-service/src/test/java/com/example/howtodoinjava/springhystrixstudentservice/SpringHystrixStudentServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.howtodoinjava.springhystrixstudentservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHystrixStudentServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
